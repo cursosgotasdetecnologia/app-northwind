a94e72a5e2c2fd1bea2ec2746ec4231623f7eb45
evertonulisystem <evertonulisystem@gmail.com>
2026-02-21 16:40:18 -0300

    feat (aula49) : cenarios exploratorios

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -45,15 +45,24 @@ test('Validar acesso caminho feliz', async ({ page }) => {
   
 });
 
+test('Verificar acesso com email invalido', async ({ page }) => {
+  await page.goto('https://northwind-test-platform.vercel.app/');
+  await page.getByTestId('email-input').click();
+  await page.getByTestId('email-input').fill('admingmail.com');
+  await page.getByTestId('password-input').click();
+  await page.getByTestId('password-input').fill('Teste@123');
+  await page.getByTestId('login-button').click();
+  await expect(page.locator('form')).toContainText('Formato de email inválido. Use: nome@dominio.com');
+  
+});
 
+test('Verificar acesso com senha invalida', async ({ page }) => {
+  await page.goto('https://northwind-test-platform.vercel.app/');
+  await page.getByTestId('email-input').click(); 
+  await page.getByTestId('email-input').fill('admin@qatest.com');
+  await page.getByTestId('password-input').click();
+  await page.getByTestId('password-input').fill('Teste@1234');
+  await page.getByTestId('login-button').click();
+  await expect(page.getByText('Email ou senha inválidos')).toBeVisible();
 
-
-
-
-/*
-email valido
-email invalido
-senha valida
-senha invalida
-
-*/
+});
