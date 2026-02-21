aa16432d97a1fcf91f0a84dbdeb0b10acf8501f8
evertonulisystem <evertonulisystem@gmail.com>
2026-02-21 17:45:36 -0300

    feat (aula 51): uso de beforeEach

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,72 +1,76 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test.describe('Login - Cenários de Validação',()=>{
-  
-test('Validar acesso sem credenciais', async ({ page }) => {
-  await page.goto('https://northwind-test-platform.vercel.app/');
-  await page.getByTestId('email-input').click();
-  await page.getByTestId('email-input').fill('');
-  await page.getByTestId('password-input').click();
-  await page.getByTestId('password-input').fill('');
-  await page.getByTestId('login-button').click();
-  await expect(page.getByText('Email e senha são obrigatórios')).toBeVisible();
+test.describe("Login - Caminho Feliz", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://northwind-test-platform.vercel.app/');
+  });
+  test("Deve realizar login com credenciais válidas", async ({ page }) => {   
+    await page.getByTestId("email-input").click();
+    await page.getByTestId("email-input").fill("admin@qatest.com");
+    await page.getByTestId("password-input").click();
+    await page.getByTestId("password-input").fill("Teste@123");
+    await page.getByTestId("login-button").click();
+    await expect(page.getByRole("heading")).toContainText("QA Automation Shop");
+    await expect(
+      page.getByRole("button", { name: "Regras do Playground" }),
+    ).toBeVisible();
+  });
 });
 
+test.describe("Login - Cenários de Erro", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://northwind-test-platform.vercel.app/');
+  });
+  test("Deve exibir erro ao tentar login sem credenciais", async ({ page }) => { 
+    await page.getByTestId("email-input").click();
+    await page.getByTestId("email-input").fill("");
+    await page.getByTestId("password-input").click();
+    await page.getByTestId("password-input").fill("");
+    await page.getByTestId("login-button").click();
+    await expect(
+      page.getByText("Email e senha são obrigatórios"),
+    ).toBeVisible();
+  });
 
-test('Validar acesso sem senha', async ({ page }) => {
-  await page.goto('https://northwind-test-platform.vercel.app/');
-  await page.getByTestId('email-input').click();
-  await page.getByTestId('email-input').fill('admin@qatest.com');
-  await page.getByTestId('password-input').click();
-  await page.getByTestId('password-input').fill('');
-  await page.getByTestId('login-button').click();
-  await expect(page.getByText('Email e senha são obrigatórios')).toBeVisible();
+  test("Deve exibir erro ao não informar senha inválida", async ({ page }) => {    
+    await page.getByTestId("email-input").click();
+    await page.getByTestId("email-input").fill("admin@qatest.com");
+    await page.getByTestId("password-input").click();
+    await page.getByTestId("password-input").fill("");
+    await page.getByTestId("login-button").click();
+    await expect(
+      page.getByText("Email e senha são obrigatórios"),
+    ).toBeVisible();
+  });
+
+  test("Deve exibir erro ao não informar um email", async ({ page }) => {  
+    await page.getByTestId("email-input").click();
+    await page.getByTestId("email-input").fill("");
+    await page.getByTestId("password-input").click();
+    await page.getByTestId("password-input").fill("Teste@123");
+    await page.getByTestId("login-button").click();
+    await expect(
+      page.getByText("Email e senha são obrigatórios"),
+    ).toBeVisible();
+  });
+
+  test("Deve exibir erro ao informar email invalido", async ({ page }) => { 
+    await page.getByTestId("email-input").click();
+    await page.getByTestId("email-input").fill("admingmail.com");
+    await page.getByTestId("password-input").click();
+    await page.getByTestId("password-input").fill("Teste@123");
+    await page.getByTestId("login-button").click();
+    await expect(page.locator("form")).toContainText(
+      "Formato de email inválido. Use: nome@dominio.com",
+    );
+  });
+
+  test("Deve exibir erro ao informar senha inválida", async ({ page }) => {   
+    await page.getByTestId("email-input").click();
+    await page.getByTestId("email-input").fill("admin@qatest.com");
+    await page.getByTestId("password-input").click();
+    await page.getByTestId("password-input").fill("Teste@1234");
+    await page.getByTestId("login-button").click();
+    await expect(page.getByText("Email ou senha inválidos")).toBeVisible();
+  });
 });
-
-
-test('Validar acesso sem email', async ({ page }) => {
-  await page.goto('https://northwind-test-platform.vercel.app/');
-  await page.getByTestId('email-input').click();
-  await page.getByTestId('email-input').fill('');
-  await page.getByTestId('password-input').click();
-  await page.getByTestId('password-input').fill('Teste@123');
-  await page.getByTestId('login-button').click();
-  await expect(page.getByText('Email e senha são obrigatórios')).toBeVisible();
-});
-
-
-test('Validar acesso caminho feliz', async ({ page }) => {
-  await page.goto('https://northwind-test-platform.vercel.app/');
-  await page.getByTestId('email-input').click();
-  await page.getByTestId('email-input').fill('admin@qatest.com');
-  await page.getByTestId('password-input').click();
-  await page.getByTestId('password-input').fill('Teste@123');
-  await page.getByTestId('login-button').click();
-  await expect(page.getByRole('heading')).toContainText('QA Automation Shop');
-  await expect(page.getByRole('button', { name: 'Regras do Playground' })).toBeVisible();
-  
-});
-
-test('Verificar acesso com email invalido', async ({ page }) => {
-  await page.goto('https://northwind-test-platform.vercel.app/');
-  await page.getByTestId('email-input').click();
-  await page.getByTestId('email-input').fill('admingmail.com');
-  await page.getByTestId('password-input').click();
-  await page.getByTestId('password-input').fill('Teste@123');
-  await page.getByTestId('login-button').click();
-  await expect(page.locator('form')).toContainText('Formato de email inválido. Use: nome@dominio.com');
-  
-});
-
-test('Verificar acesso com senha invalida', async ({ page }) => {
-  await page.goto('https://northwind-test-platform.vercel.app/');
-  await page.getByTestId('email-input').click(); 
-  await page.getByTestId('email-input').fill('admin@qatest.com');
-  await page.getByTestId('password-input').click();
-  await page.getByTestId('password-input').fill('Teste@1234');
-  await page.getByTestId('login-button').click();
-  await expect(page.getByText('Email ou senha inválidos')).toBeVisible();
-
-});
-
-})
