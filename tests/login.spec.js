b9d4fbbd6ff66aa8d5e4e6e3b1400c67cd042e8f
evertonulisystem <evertonulisystem@gmail.com>
2026-02-21 23:21:38 -0300

    feat (aula53): uso de fixtures em testes

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,14 +1,15 @@
 import { test, expect } from "@playwright/test";
+import logindata from "../fixtures/login-data.json";
 
 test.describe("Login - Caminho Feliz", () => {
   test.beforeEach(async ({ page }) => {
-     await page.goto('/');
+    await page.goto("/");
   });
   test("Deve realizar login com credenciais válidas", async ({ page }) => {
     await page.getByTestId("email-input").click();
-    await page.getByTestId("email-input").fill("admin@qatest.com");
+    await page.getByTestId("email-input").fill(process.env.USER_EMAIL);
     await page.getByTestId("password-input").click();
-    await page.getByTestId("password-input").fill("Teste@123");
+    await page.getByTestId("password-input").fill(process.env.USER_PASSWORD);
     await page.getByTestId("login-button").click();
     await expect(page.getByRole("heading")).toContainText("QA Automation Shop");
     await expect(
@@ -19,58 +20,59 @@ test.describe("Login - Caminho Feliz", () => {
 
 test.describe("Login - Cenários de Erro", () => {
   test.beforeEach(async ({ page }) => {
-   await page.goto('/');
+    await page.goto("/");
   });
   test("Deve exibir erro ao tentar login sem credenciais", async ({ page }) => {
+    const dados = logindata.emailPassNull;
+
     await page.getByTestId("email-input").click();
-    await page.getByTestId("email-input").fill("");
+    await page.getByTestId("email-input").fill(dados.email);
     await page.getByTestId("password-input").click();
-    await page.getByTestId("password-input").fill("");
+    await page.getByTestId("password-input").fill(dados.password);
     await page.getByTestId("login-button").click();
-    await expect(
-      page.getByText("Email e senha são obrigatórios"),
-    ).toBeVisible();
+    await expect(page.getByText(dados.expectMessage)).toBeVisible();
   });
 
-  test("Deve exibir erro ao não informar senha inválida", async ({ page }) => {
+  test("Deve exibir erro ao não informar senha", async ({ page }) => {
+    const dados = logindata.passwordVazio;
+
     await page.getByTestId("email-input").click();
-    await page.getByTestId("email-input").fill("admin@qatest.com");
+    await page.getByTestId("email-input").fill(dados.email);
     await page.getByTestId("password-input").click();
-    await page.getByTestId("password-input").fill("");
+    await page.getByTestId("password-input").fill(dados.password);
     await page.getByTestId("login-button").click();
-    await expect(
-      page.getByText("Email e senha são obrigatórios"),
-    ).toBeVisible();
+    await expect(page.getByText(dados.expectMessage)).toBeVisible();
   });
 
   test("Deve exibir erro ao não informar um email", async ({ page }) => {
-    await page.getByTestId("email-input").click();
-    await page.getByTestId("email-input").fill("");
+    const dados = logindata.emailVazio;
+
+    await page.getByTestId("email-input").fill(dados.email);
     await page.getByTestId("password-input").click();
-    await page.getByTestId("password-input").fill("Teste@123");
+    await page.getByTestId("password-input").fill(dados.password);
     await page.getByTestId("login-button").click();
-    await expect(
-      page.getByText("Email e senha são obrigatórios"),
-    ).toBeVisible();
+    await expect(page.getByText(dados.expectMessage)).toBeVisible();
   });
 
   test("Deve exibir erro ao informar email invalido", async ({ page }) => {
+    const dados = logindata.invalidEmail;
+
     await page.getByTestId("email-input").click();
-    await page.getByTestId("email-input").fill("admingmail.com");
+    await page.getByTestId("email-input").fill(dados.email);
     await page.getByTestId("password-input").click();
-    await page.getByTestId("password-input").fill("Teste@123");
+    await page.getByTestId("password-input").fill(dados.password);
     await page.getByTestId("login-button").click();
-    await expect(page.locator("form")).toContainText(
-      "Formato de email inválido. Use: nome@dominio.com",
-    );
+    await expect(page.locator("form")).toContainText(dados.expectMessage);
   });
 
   test("Deve exibir erro ao informar senha inválida", async ({ page }) => {
+    const dados = logindata.invalidPassword;
+
     await page.getByTestId("email-input").click();
-    await page.getByTestId("email-input").fill("admin@qatest.com");
+    await page.getByTestId("email-input").fill(dados.email);
     await page.getByTestId("password-input").click();
-    await page.getByTestId("password-input").fill("Teste@1234");
+    await page.getByTestId("password-input").fill(dados.password);
     await page.getByTestId("login-button").click();
-    await expect(page.getByText("Email ou senha inválidos")).toBeVisible();
+    await expect(page.getByText(dados.expectMessage)).toBeVisible();
   });
 });
