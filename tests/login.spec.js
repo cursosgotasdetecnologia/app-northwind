bb0953a7da56e38487cc09bb81beeec08821ae50
evertonulisystem <evertonulisystem@gmail.com>
2026-02-22 10:34:08 -0300

    feat (aula 54): refatorado sem redundâncias

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -5,16 +5,18 @@ test.describe("Login - Caminho Feliz", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("/");
   });
-  test("Deve realizar login com credenciais válidas", async ({ page }) => {
-    await page.getByTestId("email-input").click();
-    await page.getByTestId("email-input").fill(process.env.USER_EMAIL);
-    await page.getByTestId("password-input").click();
+
+test("Deve realizar login com credenciais válidas", async ({ page }) => {
+
+    const DASHBOARD_TITULO = "QA Automation Shop"
+    const RULLES_BUTTON = "Regras do Playground"
+
+    await page.getByTestId("email-input").fill(process.env.USER_EMAIL);    
     await page.getByTestId("password-input").fill(process.env.USER_PASSWORD);
     await page.getByTestId("login-button").click();
-    await expect(page.getByRole("heading")).toContainText("QA Automation Shop");
+    await expect(page.getByRole("heading")).toContainText(DASHBOARD_TITULO);
     await expect(
-      page.getByRole("button", { name: "Regras do Playground" }),
-    ).toBeVisible();
+      page.getByRole("button", { name:RULLES_BUTTON })).toBeVisible();
   });
 });
 
@@ -25,9 +27,7 @@ test.describe("Login - Cenários de Erro", () => {
   test("Deve exibir erro ao tentar login sem credenciais", async ({ page }) => {
     const dados = logindata.emailPassNull;
 
-    await page.getByTestId("email-input").click();
-    await page.getByTestId("email-input").fill(dados.email);
-    await page.getByTestId("password-input").click();
+    await page.getByTestId("email-input").fill(dados.email);   
     await page.getByTestId("password-input").fill(dados.password);
     await page.getByTestId("login-button").click();
     await expect(page.getByText(dados.expectMessage)).toBeVisible();
@@ -35,10 +35,8 @@ test.describe("Login - Cenários de Erro", () => {
 
   test("Deve exibir erro ao não informar senha", async ({ page }) => {
     const dados = logindata.passwordVazio;
-
-    await page.getByTestId("email-input").click();
-    await page.getByTestId("email-input").fill(dados.email);
-    await page.getByTestId("password-input").click();
+    
+    await page.getByTestId("email-input").fill(dados.email);  
     await page.getByTestId("password-input").fill(dados.password);
     await page.getByTestId("login-button").click();
     await expect(page.getByText(dados.expectMessage)).toBeVisible();
@@ -47,8 +45,7 @@ test.describe("Login - Cenários de Erro", () => {
   test("Deve exibir erro ao não informar um email", async ({ page }) => {
     const dados = logindata.emailVazio;
 
-    await page.getByTestId("email-input").fill(dados.email);
-    await page.getByTestId("password-input").click();
+    await page.getByTestId("email-input").fill(dados.email);   
     await page.getByTestId("password-input").fill(dados.password);
     await page.getByTestId("login-button").click();
     await expect(page.getByText(dados.expectMessage)).toBeVisible();
@@ -57,9 +54,7 @@ test.describe("Login - Cenários de Erro", () => {
   test("Deve exibir erro ao informar email invalido", async ({ page }) => {
     const dados = logindata.invalidEmail;
 
-    await page.getByTestId("email-input").click();
-    await page.getByTestId("email-input").fill(dados.email);
-    await page.getByTestId("password-input").click();
+    await page.getByTestId("email-input").fill(dados.email);   
     await page.getByTestId("password-input").fill(dados.password);
     await page.getByTestId("login-button").click();
     await expect(page.locator("form")).toContainText(dados.expectMessage);
@@ -68,9 +63,7 @@ test.describe("Login - Cenários de Erro", () => {
   test("Deve exibir erro ao informar senha inválida", async ({ page }) => {
     const dados = logindata.invalidPassword;
 
-    await page.getByTestId("email-input").click();
     await page.getByTestId("email-input").fill(dados.email);
-    await page.getByTestId("password-input").click();
     await page.getByTestId("password-input").fill(dados.password);
     await page.getByTestId("login-button").click();
     await expect(page.getByText(dados.expectMessage)).toBeVisible();
