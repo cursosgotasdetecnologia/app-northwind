4e9083b7b6374e8cae5782f6d6a157b87223f361
evertonulisystem <evertonulisystem@gmail.com>
2026-02-24 12:50:18 -0300

    feat (auka61): criando um  monstro

--- pages/CadastroPage.js
+++ pages/CadastroPage.js
@@ -8,32 +8,37 @@ class CadastroPage {
     this.campoSenha = page.getByTestId("password-input");
     this.campoConfirmaSenha = page.getByTestId("confirm-password-input");
     this.botaoCadastrar = page.getByRole("button", { name: "Criar Conta" });
-    this.mensagemSucesso = page.getByText("Cadastro realizado com sucesso! Redirecionando...");    
+    this.mensagemSucesso = page.getByText(
+      "Cadastro realizado com sucesso! Redirecionando...",
+    );
+    this.mensagemNomeCurto = page.locator('text=Nome deve ter no mínimo 3 caracteres');    
+    this.mensagemNomeComNumero = page.getByText('Nome deve conter apenas letras e espaços')
+    this.mensagemEmailSemArroba = page.getByText('E-mail inválido')
+    this.mensagemEmailSemDominio = page.getByText('E-mail inválido')
+    this.mensagemEmailSemIdentificacao = page.getByText('E-mail inválido')
+    this.mensagemSenhaSemMaiuscula = page.getByText('E-mail inválido')
   }
 
   //Métodos de ação
 
-  async preencherNome(nome) {    
+  async preencherNome(nome) {
     await this.campoNome.fill(nome);
-   // await this.campoNome.blur();
+    // await this.campoNome.blur();
   }
 
   async preencherEmail(email) {
     await this.campoEmail.fill(email);
   }
 
-    async preencherSenha(senha) {
-    await this.campoSenha.fill(senha);  
+  async preencherSenha(senha) {
+    await this.campoSenha.fill(senha);
   }
-   async preencherConfirmarSenha(senha) {
+  async preencherConfirmarSenha(senha) {
     await this.campoConfirmaSenha.fill(senha);
   }
 
-  async clicarCadastrar() {  
-      await this.botaoCadastrar.click();
+  async clicarCadastrar() {
+    await this.botaoCadastrar.click();
   }
-
-
-
 }
 module.exports = CadastroPage;
--- tests/cadastroLogin.spec.js
+++ tests/cadastroLogin.spec.js
@@ -1,17 +1,75 @@
-import { test, expect, describe } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 import CadastroPage from "../pages/CadastroPage";
 
+test.describe("Cadastro de Usuário", () => {
+  let cadastroPage;
 
-test('Cadastro de Usuário', async ({ page }) => {
-  const cadastroPage = new CadastroPage(page);
+  test.beforeEach(async ({ page }) => {
+    cadastroPage = new CadastroPage(page);
+    await page.goto("/");
+    await page.getByRole("link", { name: "Cadastre-se" }).click();
+  });
 
-  await page.goto('https://northwind-test-platform.vercel.app/');
-  await page.getByRole('link', { name: 'Cadastre-se' }).click();
-  await cadastroPage.preencherNome('Marieanate Julianae Car');
-  await cadastroPage.preencherEmail('marireeetuliane@gmail.com');
-  await cadastroPage.preencherSenha('SenhaForte@123');
-  await cadastroPage.preencherConfirmarSenha('SenhaForte@123');
-  await cadastroPage.clicarCadastrar();
-  await expect(cadastroPage.mensagemSucesso).toBeVisible()
+  const dados = require("../fixtures/dados-cadastro-login.json");
 
+  test.describe("Validação de Nome", () => {
+    test("Deve exibir erro quando nome tiver menos de 3 caracteres", async () => {
+      const cenario = dados.nomeCurto;
+      await cadastroPage.preencherNome(cenario.nome);
+      await expect(cadastroPage.mensagemNomeCurto).toBeVisible();
+    });
+
+    test("Deve exibir erro quando nome tiver números", async () => {
+      const cenario = dados.nomeComNumeros;
+      await cadastroPage.preencherNome(cenario.nome);
+      await expect(cadastroPage.mensagemNomeComNumero).toBeVisible();
+    });
+  });
+
+  test.describe("Validação de Email", () => {
+
+  test("Deve exibir erro quando email não ter o @", async () => {
+    const cenario = dados.emailSemArroba;
+    await cadastroPage.preencherNome(cenario.nome);
+    await cadastroPage.preencherEmail(cenario.email);
+    await expect(cadastroPage.mensagemEmailSemArroba).toBeVisible();
+  });
+  test("Deve exibir erro quando email não tiver o domínio após o @", async () => {
+    const cenario = dados.emailSemDominio;
+    await cadastroPage.preencherNome(cenario.nome);
+    await cadastroPage.preencherEmail(cenario.email);
+    await expect(cadastroPage.mensagemEmailSemDominio).toBeVisible();
+  });
+  test("Deve exibir erro quando email não tiver a primeira parte antes do @", async () => {
+    const cenario = dados.emailSemIdentificacao;
+    await cadastroPage.preencherNome(cenario.nome);
+    await cadastroPage.preencherEmail(cenario.email);
+    await expect(cadastroPage.mensagemEmailSemIdentificacao).toBeVisible();
+  });
 });
+
+  test.describe("Validação de Senha", () => {});
+    test("Deve exibir erro quando email não tiver a primeira parte antes do @", async () => {
+      const cenario = dados.senhaSemMaiusculas;
+      await cadastroPage.preencherNome(cenario.nome);
+      await cadastroPage.preencherEmail(cenario.email);
+      await expect(cadastroPage.mensagemSenhaSemMaiuscula).toBeVisible();
+    });
+  });
+
+
+
+
+// test('Cadastro de Usuário', async ({ page }) => {
+//   const cadastroPage = new CadastroPage(page);
+
+//   await page.goto('https://northwind-test-platform.vercel.app/');
+//   await page.getByRole('link', { name: 'Cadastre-se' }).click();
+//   await cadastroPage.preencherNome('Marieanaute Julianae Car');
+//   await cadastroPage.preencherEmail('marireeeutuliane@gmail.com');
+//   await cadastroPage.preencherSenha('SenhaForte@123');
+//   await cadastroPage.preencherConfirmarSenha('SenhaForte@123');
+//   await cadastroPage.clicarCadastrar();
+//   await expect(cadastroPage.mensagemSucesso).toBeVisible()
+
+// });
